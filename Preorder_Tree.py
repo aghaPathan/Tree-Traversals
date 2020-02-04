2311c8825747536e48af7985f959bcc71e716cfd
Agha Awais <agha.awais63@live.com>
2020-02-04 12:49:18 +0500

    Add files via upload

---
+++ Preorder_Tree.py
@@ -0,0 +1,38 @@
+
+from Tree import Node
+
+tree_order = []
+
+# Creating a tree
+root = Node(1)
+root.left_child = Node(2)
+root.right_child = Node(3)
+root.left_child.left_child = Node(4)
+root.left_child.right_child = Node(5)
+root.right_child.left_child = Node(6)
+root.right_child.right_child = Node(7)
+root.left_child.left_child.left_child = Node(8)
+root.left_child.left_child.left_child.left_child = Node(9)
+root.left_child.left_child.left_child.right_child = Node(10)
+
+class In_Order():
+    def __init__(self, root):
+        """
+            This class is used to demonstrate Pre-Order Tree Traversal
+        """
+        self.tree_order = []
+        self._process(root)
+
+    def _process(self, root):
+        """ This method processes the logic """
+        if root:
+            self.tree_order.append(root.node_data)
+            self._process(root.left_child)
+            self._process(root.right_child)
+
+
+in_order_tree = In_Order(root)
+# Printing Tree Order
+print(in_order_tree.tree_order)
+
+
---
+++ Tree.py
@@ -0,0 +1,11 @@
+
+
+class Node():
+    def __init__(self, value):
+        """
+        This class is used to create a tree class of a Node and its right child
+         and left child
+        """
+        self.node_data = value
+        self.right_child = None
+        self.left_child = None
